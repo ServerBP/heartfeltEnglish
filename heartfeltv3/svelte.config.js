bcbb37f11e815fb69a45f103672c19b1aef4948b
Luna <palas.balazs@gmail.com>
2026-04-10 20:46:11 +0200

    Add Vercel adapter support and update build scripts for deployment

--- heartfeltv3/svelte.config.js
+++ heartfeltv3/svelte.config.js
@@ -1,5 +1,8 @@
 import azure from 'svelte-adapter-azure-swa';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isVercel = process.env.VERCEL === '1' || process.env.DEPLOY_TARGET === 'vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +14,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: azure()
+		adapter: isVercel ? vercel() : azure()
 	}
 };
 
